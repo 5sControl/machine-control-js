453255069859bc252b4ee24fdb068a024335d34f
artsiom <artsiomzhuchkou2401@gmail.com>
2023-06-07 16:37:59 +0300

    fix ips

--- algorithm.js
+++ algorithm.js
@@ -7,8 +7,7 @@ const {camera_url, folder, server_url} = process.env;
 const run = async () => {
     const camera = new Camera()
     const reqBody = {algorithm: 'machine_control', camera_url, server_url, extra: []}
-    const SERVER_IP = server_url.split('//')[1]
-    const cameraInterval = await camera.init(reqBody, false, SERVER_IP)
+    const cameraInterval = await camera.init(reqBody, false, folder)
     const createdAlgorithm = new Machine(cameraInterval.camera, 'machine_control', [])
     const algorithmInterval = await createdAlgorithm.start(cameraInterval.camera)
 }
