8a1141b6303347ce7fb1bae4e13aa3251bc5550c
a-matyukh <amatyukh@gmail.com>
2023-07-25 10:01:50 +0300

    set envs without docker-compouse

--- src/globals.js
+++ src/globals.js
@@ -2,6 +2,9 @@ const {YMD, HMS} = require('./utils/Date')
 const {checkDirs} = require('./utils/Path')
 global.dispatcher = require('./utils/Dispatcher')
 
+if (!process.env.camera_url) process.env.camera_url = "10.20.100.40"
+if (!process.env.folder) process.env.folder = "images/10.20.100.40"
+if (!process.env.server_url) process.env.server_url = "http://172.16.101.100"
 process.env.N_CPUS = require('os').cpus().length
 process.env.currentDebugFolder = `debug/operation-control/${YMD(new Date())}`
 global.WORKSPACE_ZONE = [280, 0, 1200, 900]
