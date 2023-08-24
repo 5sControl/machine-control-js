8dbe30ad88b3301614eca004ec8a7f38edb27ffc
slanx <andraffal@gmail.com>
2023-08-24 16:25:41 +0300

    fix: fix bug with socket

--- src/Translation/index.js
+++ src/Translation/index.js
@@ -1,5 +1,6 @@
 const io = require('socket.io-client')
 const socketURL = process.env.socket_server || `${process.env.server_url}:3456`
+const camera = process.env.folder?.split('/')[1] || '192.168.1.110'
 const socket = io(socketURL)
 const Snapshot = require('./Snapshot.js')
 
@@ -48,8 +49,10 @@ class Translation {
     constructor() {
         socket.on("connect", async () => {
             console.log(`Connected to the socket server: ${socketURL}`)
-            socket.on("snapshot_updated", async ({screenshot}) => this.update(screenshot))
         })
+        socket.on('snapshot_updated', async ({ camera_ip, screenshot }) => {
+            if (camera_ip === camera) this.update(screenshot);
+        });
     }
 
 }
