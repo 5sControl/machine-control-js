165ae8640e02436b96732520b24d5479955c995a
a-matyukh <amatyukh@gmail.com>
2023-08-29 06:02:44 +0300

    extract cutRegionFromBlob

--- src/Detector/index.js
+++ src/Detector/index.js
@@ -1,5 +1,5 @@
 const loadYoloNAS = require('./models/yolo-nas')
-const {createCanvas, Image} = require("@napi-rs/canvas")
+const { cutRegionFromBlob } = require('../utils')
 
 class Detector {
     model
@@ -17,23 +17,6 @@ class Detector {
         console.timeEnd("detect")
         return detections
     }
-}
-
-async function cutRegionFromBlob(buffer, region) {
-    const [cHeight, cWidth] = [1080, 1920]
-    let canvas = createCanvas(cWidth, cHeight)
-    let ctx = canvas.getContext('2d')
-    const image = new Image()
-    image.src = buffer
-    ctx.drawImage(image, 0, 0)
-    const [x, y, width, height] = region
-    const OFFSET = 20
-    let cuttedWorker = ctx.getImageData(x - OFFSET, y - OFFSET, width + OFFSET, height + OFFSET)
-    let newCan = createCanvas(width + OFFSET, height + OFFSET)
-    let newCtx = newCan.getContext('2d')
-    newCtx.putImageData(cuttedWorker, 0, 0)
-    const croppedBlob = await newCan.encode('jpeg', 90)
-    return croppedBlob
 }
 
 const detector = new Detector()
