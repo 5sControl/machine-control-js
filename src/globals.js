346ead5486caf20ffd447fad7b3263af5b2b96e0
a-matyukh <amatyukh@gmail.com>
2023-09-05 10:29:16 +0300

    ref: remove fakeExtra

--- src/globals.js
+++ src/globals.js
@@ -4,4 +4,4 @@ const {checkDirs} = require('./utils/Path')
 checkDirs([process.env.folder])
 
 process.env.camera_ip = process.env.folder.split('/')[1]
-if (!process.env.extra) process.env.extra = require('./utils/fakeExtra.js')
+if (!process.env.extra) process.env.extra = `[{"coords":[{"x1":280,"x2":1480,"y1":200,"y2":1000,"zoneId":1,"zoneName":"zone zone1"},{"x1":1564,"x2":1914,"y1":783,"y2":1073,"zoneId":2,"zoneName":"zone zone2"}]}]`
