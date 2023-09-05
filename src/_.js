8ef750cf0d09628f95519d4a073a4d94b6e8df8f
a-matyukh <amatyukh@gmail.com>
2023-09-05 18:18:22 +0300

    ref: remove dispatcher from _.js

--- src/_.js
+++ src/_.js
@@ -2,9 +2,9 @@ require('./globals')
 require('./Translation')
 require('./Control')
 
-dispatcher.emit("container started", { message: `
+console.log("container started", `
+time: ${new Date()}
 server_url: ${process.env.server_url}
 camera_ip: ${process.env.camera_ip}
 extra: ${process.env.extra}
-`
-})
+`)
