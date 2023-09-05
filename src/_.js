52258540664ef5f5bd7a19a91022326033803745
a-matyukh <amatyukh@gmail.com>
2023-09-05 10:06:26 +0300

    ref: remove camera_url

--- src/_.js
+++ src/_.js
@@ -4,7 +4,6 @@ require('./Control')
 require('./Report')
 
 dispatcher.emit("container started", { message: `
-camera_url: ${process.env.camera_url}
 folder: ${process.env.folder}
 server_url: ${process.env.server_url}
 socket_server: ${process.env.socket_server}
--- src/globals.js
+++ src/globals.js
@@ -2,7 +2,6 @@ const {checkDirs} = require('./utils/Path')
 const fakeExtra = require('./utils/fakeExtra.js')
 global.dispatcher = require('./utils/Dispatcher')
 
-if (!process.env.camera_url) process.env.camera_url = "10.20.100.40"
 if (!process.env.server_url) process.env.server_url = "http://172.16.101.100"
 if (!process.env.folder) process.env.folder = "images/10.20.100.40"
 checkDirs([process.env.folder])
