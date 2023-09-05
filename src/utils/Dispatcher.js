26706ecfa440ce11122057177e225d27e9a1ac65
a-matyukh <amatyukh@gmail.com>
2023-09-05 18:07:18 +0300

    remove utils/Date

--- src/utils/Dispatcher.js
+++ src/utils/Dispatcher.js
@@ -1,5 +1,24 @@
 const {EventEmitter} = require('events')
-const {Timestamp} = require('./Date')
+
+function Timestamp() {
+	const date = new Date()
+	const padTo2Digits = (num) => num.toString().padStart(2, '0')
+	return (
+		[
+            date.getFullYear(),
+            padTo2Digits(date.getMonth() + 1),
+            padTo2Digits(date.getDate()),
+		].join('-')
+            + ' ' +
+		[
+            padTo2Digits(date.getHours()),
+            padTo2Digits(date.getMinutes()),
+            padTo2Digits(date.getSeconds())
+		].join(':')
+		+ '.' +
+		date.getMilliseconds()
+	)
+}
 
 class Dispatcher extends EventEmitter {
     emit(eventName, options) {
