99b1cf7d23832c99ea8570d39ff19dfeb386f205
a-matyukh <amatyukh@gmail.com>
2023-09-05 10:07:57 +0300

    ref: remove socket_server

--- src/Translation/index.js
+++ src/Translation/index.js
@@ -1,5 +1,5 @@
 const io = require('socket.io-client')
-const socketURL = process.env.socket_server || `${process.env.server_url}:3456`
+const socketURL = `${process.env.server_url}:3456`
 const camera = process.env.folder?.split('/')[1] || '192.168.1.110'
 const socket = io(socketURL)
 const Snapshot = require('./Snapshot.js')
--- src/_.js
+++ src/_.js
@@ -6,6 +6,5 @@ require('./Report')
 dispatcher.emit("container started", { message: `
 folder: ${process.env.folder}
 server_url: ${process.env.server_url}
-socket_server: ${process.env.socket_server}
 `
 })
